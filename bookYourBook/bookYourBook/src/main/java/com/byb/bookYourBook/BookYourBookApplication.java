85bdaa965481436b6b5ff566f8c891d883c15684
SturdyJoshi <nirazjoshi007@gmail.com>
2023-08-01 18:02:52 +0530

    added the spring boot project

---
+++ bookYourBook/bookYourBook/src/main/java/com/byb/bookYourBook/BookYourBookApplication.java
@@ -0,0 +1,13 @@
+package com.byb.bookYourBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourBookApplication.class, args);
+	}
+
+}
---
+++ bookYourBook/bookYourBook/src/test/java/com/byb/bookYourBook/BookYourBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byb.bookYourBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
